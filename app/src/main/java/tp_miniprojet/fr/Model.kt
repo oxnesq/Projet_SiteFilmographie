a3b2d3f8e88a7d08c6c470b67a9bda8f24b645c2
oxnesq <oceane.copub@gmail.com>
2024-11-13 14:28:43 +0100

    modèle en place

--- app/src/main/java/tp_miniprojet/fr/Model.kt
+++ app/src/main/java/tp_miniprojet/fr/Model.kt
@@ -104,3 +104,22 @@ data class Genre(
 )
 
 
+data class CollectionModel(
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
+
+data class Result(
+    val adult: Boolean,
+    val backdrop_path: String,
+    val id: Int,
+    val name: String,
+    val original_language: String,
+    val original_name: String,
+    val overview: String,
+    val poster_path: String
+)
+
+
